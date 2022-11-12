ed317d982c933cb5368224aba305ad80074341d7
maryem.ABDENNABI <maryem.abdennabi@esprit.tn>
2022-11-12 16:45:40 +0100

    initial commit

--- src/app/shared/Service/Reglement.service.ts
+++ src/app/shared/Service/Reglement.service.ts
@@ -6,7 +6,7 @@ import {HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class ReglementService {
-  readonly API_URL = 'http://localhost:8089/SpringMVC/reglement';
+  readonly API_URL = 'http://169.254.103.182:8089/SpringMVC/reglement';
 
   constructor(private httpClient: HttpClient) {
   }
--- src/app/shared/Service/Stock.service.ts
+++ src/app/shared/Service/Stock.service.ts
@@ -7,7 +7,7 @@ import { HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class StockService {
-  readonly API_URL = 'http://localhost:8089/SpringMVC/stock';
+  readonly API_URL = 'http://169.254.103.182:8089/SpringMVC/stock';
 
   constructor(private httpClient: HttpClient) { }
 
